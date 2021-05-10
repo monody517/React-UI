218549f83feda0d43316a3c14d54aa663ad4f8cf
Monody517 <2737716714@qq.com>
2021-05-10 15:17:47 +0800

    将类型声明放入.d.ts文件

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
   entry: {
     JUI: "./lib/index.tsx",
   },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
   output: {
     path: path.resolve(__dirname, "dist/lib"),
     library: "JUI",
@@ -23,4 +26,18 @@ module.exports = {
       template: "index.html",
     }),
   ],
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "react-dom",
+      root: "ReactDOM",
+    },
+  },
 };
