a5bc450e617488e7c40ea4bbd094045ff28b0200
Monody517 <2737716714@qq.com>
2021-05-11 14:10:11 +0800

    装loader使scss生效

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 // import wechat from './icons/wechat.svg'
 import './importIcons.js'
+import './icon.scss'
 
 
 interface IconProps {
@@ -8,8 +9,8 @@ interface IconProps {
 }
 
 const Icon:React.FunctionComponent<IconProps> = (props) => {
-    return(
-        <svg>
+    return(      
+        <svg className="icon">
             <use xlinkHref={`#${props.name}`}></use>
         </svg>
     )
