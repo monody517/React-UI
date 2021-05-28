ccd5f8a577c2bf3372c03086c731f7c3cb38bbf9
monody517 <2737716714@qq.com>
2021-05-28 17:37:42 +0800

    icon.example应用example

--- lib/icons/icons.explame.tsx
+++ lib/icons/icons.explame.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import Icon from './icon'
+import Example from '../example/example'
 
 export default function(){
     const fn:React.MouseEventHandler= (e) => {
         console.log(e.target)
     }
     return(
-        <div>
-            <div>
+            <Example title='图标' description='图标显示，支持各种事件'>
                 <Icon 
                 onClick={fn} 
                 onMouseEnter={()=>console.log('enter')}
                 onMouseLeave={()=>console.log('leave')}
                 name="pay"></Icon>
-            </div>
-        </div>
+            </Example>
     )
 }
